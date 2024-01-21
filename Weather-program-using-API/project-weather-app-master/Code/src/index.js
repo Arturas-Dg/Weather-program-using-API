0789d66bc2ac5cdb46f00afd1421dada66ba6aa2
Arturas <Arturaz.dg@gmail.com>
2024-01-21 18:03:59 +0200

    changed layout

--- Weather-program-using-API/project-weather-app-master/Code/src/index.js
+++ Weather-program-using-API/project-weather-app-master/Code/src/index.js
@@ -1,16 +1,19 @@
 const weatherApi =
-  "https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=ba39a634841efd509a1c7b07a0c59e68";
+  "https://api.openweathermap.org/data/2.5/weather?q=madagascar&units=metric&APPID=ba39a634841efd509a1c7b07a0c59e68";
 const weatherForecast =
   "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=ba39a634841efd509a1c7b07a0c59e68";
 const temperatureDescription = document.getElementById("temp-description");
+
+const country = document.getElementById("country-name");
 const sunsetSunrise = document.getElementById("sunset-sunrise");
 const cloud = document.getElementById("cloud-svg");
 const sunglasses = document.getElementById("sunglasses-svg");
 const umbrella = document.getElementById("umbrella-svg");
-
-const country = document.getElementById("country-name");
+const snow = document.getElementById("snow-svg");
 const forecast = document.getElementById("forecast");
-cloud.style.display = "block";
+const body = document.body;
+
+body.style.backgroundColor = "black";
 fetch(weatherApi)
   .then((weatherResponse) => {
     return weatherResponse.json();
@@ -18,10 +21,11 @@ fetch(weatherApi)
   .then((weatherData) => {
     console.log(`Response: ${weatherData.weather[0].description}`);
     country.innerHTML += `<div>${weatherData.name}</div>`;
+    country.style.color = "black";
     let temperature = weatherData.main.temp;
 
+    temperatureDescription.innerHTML += `<div>${weatherData.weather[0].description} | </div>`;
     temperatureDescription.innerHTML += `<div class="temp">${temperature}°</div>`;
-    temperatureDescription.innerHTML += `<div>${weatherData.weather[0].description}</div>`;
 
     const sunrise = weatherData.sys.sunrise;
     const sunset = weatherData.sys.sunset;
@@ -41,15 +45,20 @@ fetch(weatherApi)
     sunsetSunrise.innerHTML += `<div>Sunset: ${sunsetTime}</div>`;
     if (weatherData.weather[0].description.includes("cloud")) {
       cloud.style.display = "block";
-      body.style.color = "white";
+      document.body.style.color = "red";
+      document.body.style.backgroundColor = "#F5EEE6";
     } else if (weatherData.weather[0].description.includes("snow")) {
-      body.style.background = "CadetBlue";
+      snow.style.display = "block";
+      document.body.style.color = "#F05454";
+      body.style.backgroundColor = "#F5F5F5";
     } else if (weatherData.weather[0].description.includes("clear")) {
       sunglasses.style.display = "block";
+      document.body.style.color = "#557C55";
       document.body.style.backgroundColor = "AntiqueWhite";
     } else if (weatherData.weather[0].description.includes("rain")) {
       umbrella.style.display = "block";
-      document.body.style.backgroundColor = "LightSkyBlue";
+      document.body.style.color = "#280274";
+      document.body.style.backgroundColor = "#DCF2F1";
     }
   });
 fetch(weatherForecast)
